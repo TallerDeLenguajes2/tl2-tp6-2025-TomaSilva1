c6b6d8ab8d212e81f77368ec6f819c3f44b15329
TomasSilva1 <tomasilva302@gmail.com>
2025-10-18 13:10:54 -0300

    tp6-modificado

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.Data.Sqlite;
-string connectionString = "Data Source=base_test.db;";
+string connectionString = "Data Source=nueva.db;";
 
 // Crear conexión a la base de datos
 using (SqliteConnection connection = new SqliteConnection(connectionString))
@@ -15,13 +15,14 @@ using (SqliteConnection connection = new SqliteConnection(connectionString))
         Console.WriteLine("Tabla 'productos' creada o ya existe.");
     }
     
-    // Insertar datos
-    string insertQuery = "INSERT INTO productos (nombre, precio) VALUES ('Manzana', 0.50), ('Banana', 0.30)";
+    // Insertar datos 
+    /*
+    string insertQuery = "INSERT INTO presupuestos (nombreDestinatario, fechaCreacion) VALUES ('Tomas', '2025-10-25'), ('Facundo', '2025-10-26')";
             using (SqliteCommand insertCmd = new SqliteCommand(insertQuery, connection))
             {
                 insertCmd.ExecuteNonQuery();
-                Console.WriteLine("Datos insertados en la tabla 'productos'.");
-            }
+                Console.WriteLine("Datos insertados en la tabla 'presupuestos'.");
+            }*/
     // Leer datos
             string selectQuery = "SELECT * FROM productos";
             using (SqliteCommand selectCmd = new SqliteCommand(selectQuery, connection))
@@ -30,9 +31,20 @@ using (SqliteConnection connection = new SqliteConnection(connectionString))
                 Console.WriteLine("Datos en la tabla 'productos':");
                 while (reader.Read())
                 {
-                    Console.WriteLine($"ID: {reader["id"]}, Nombre: {reader["nombre"]}, Precio: {reader["precio"]}");
+                    Console.WriteLine($"ID: {reader["id_prod"]}, Nombre: {reader["descripcion"]}, Precio: {reader["precio"]}");
                 }
             }
+    //leer presupuestos
+    string selectQuery2 = "SELECT * FROM presupuestos";
+    using (SqliteCommand selectCmd = new SqliteCommand(selectQuery2, connection))
+    using (SqliteDataReader reader = selectCmd.ExecuteReader())
+    {
+        Console.WriteLine("Datos de tabla presupuestos");
+        while (reader.Read())
+        {
+            Console.WriteLine($"ID: {reader["idPresupuesto"]}, Destinatario: {reader["nombreDestinatario"]}, fecha: {reader["fechaCreacion"]}");
+        }
+    }
 
-            connection.Close();
+        connection.Close();
 }
